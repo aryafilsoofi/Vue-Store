7f773caafcf48d4173438d3a4ad436bf33066d52
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2019-11-25 15:49:18 -0500

    created api/app/ for server.js

---
+++ api/app/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var path = require('path');
+var serveStatic = require('serve-static');
+app = express();
+//here we are configuring dist to serve app files
+app.use(serveStatic(path.join(__dirname + "/dist")));
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started ' + port);
